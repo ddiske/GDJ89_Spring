a5e0a0f052bf2e8163a7928d85d51608ede39cdf
ddiske <ddiske@naver.com>
2025-02-17 15:22:41 +0900

    Day35_Init

---
+++ src/main/java/com/root/app/products/ProductController.java
@@ -0,0 +1,27 @@
+package com.root.app.products;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller 
+@RequestMapping(value = "/products/*")
+public class ProductController {
+	
+	@RequestMapping(value = "list", method = RequestMethod.GET)
+	public String getList() throws Exception {
+		System.out.println("Product List");
+		
+		return "products/list";
+	}
+	
+	@RequestMapping(value = "detail", method = RequestMethod.GET)
+	public String getDetail() throws Exception {
+		System.out.println("Product Detail");
+		
+		return "products/detail";
+	}
+	
+	
+
+}
